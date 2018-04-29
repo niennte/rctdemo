580e00b8665faa7495ad9b85f514c9668afee436
zencam@imac <zen.camera.project@gmail.com>
2018-04-28 20:18:19 -0400

    Add data fields.

---
+++ src/ProjectItemLinks.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class ProjectItemLinks extends Component {
+
+    constructor(props) {
+        super(props);
+
+        console.log(props);
+
+        this.state = {
+            project: props.project
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+       this.setState({
+           project: nextProps.project
+       });
+    }
+
+    render() {
+
+        const {
+            project: {
+                kind,
+                tech,
+                github,
+                demos,
+                relatedProjects
+                },
+            referredBy } = this.state;
+
+            console.log(this.state);
+
+
+        if (kind && kind.length) {
+            // rename Links into Meta
+            // <ProjectItemTextList list={kind} />
+            // <ProjectItemLinkList list={kind} />
+        }
+
+
+        return(
+            <dl>
+
+                <dt>Kind: </dt>
+                <dd>
+                    <ul>
+                        {kind && kind.length ?
+
+                            kind.map((item, i) => {
+                                return(
+                                    <li>{item}</li>
+                                )
+                            }) : ""
+                        }
+                    </ul>
+                </dd>
+
+                <dt>Tech: </dt>
+                <dd>
+                    <ul>
+                        {tech && tech.length ?
+
+                            tech.map((item, i) => {
+                                return(
+                                    <li>{item}</li>
+                                )
+                            }) : ""
+                        }
+                    </ul>
+                </dd>
+
+                <dt>{"{demo}"}</dt>
+                <dd>
+                    <ul>
+                        {demos && demos.length ?
+                            demos.map((item, i) => {
+                                return(
+                                    <Link
+                                        to={item.URL}
+                                        target="_blank"
+                                        >
+                                        {item.label}
+                                    </Link>
+                                )
+                            }) : ""
+                        }
+                    </ul>
+                </dd>
+
+                <dt>{"{code}"}</dt>
+                <dd>
+                    <ul>
+                        {github && github.length ?
+                            github.map((item, i) => {
+                                return(
+                                    <Link
+                                        to={item.URL}
+                                        target="_blank"
+                                        >
+                                        {item.label}
+                                    </Link>
+                                )
+                            }) : ""
+                        }
+                    </ul>
+                </dd>
+            </dl>
+        );
+    }
+}
+
+export default ProjectItemLinks;
--- src/projectData.js
+++ src/projectData.js
@@ -5,47 +5,249 @@ const projectData = [
     "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
     "image": "/img/analysis-business.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['Relational design', 'Application architecture'],
+    "tech": ['YII 2' ,'mySQL'],
+    "github": [
+      {
+        "label": "Common models",
+        "URL": "https://github.com/niennte/isk2/commit/6e2ee0d3060130659068bb414838e900dd4867cb"
+      },
+      {
+        "label": "Common models with relationships and business rules",
+        "URL": "https://github.com/niennte/isk2/tree/master/common/models"
+      },
+      {
+        "label": "Entity Manager controllers",
+        "URL": "URL"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Entity manager",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com:8080"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
   {
     "id": 2,
     "title": "Usability analysis",
     "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
-    "image": "/img/analysis-usability.png",
+    "image": "/img/analysis-usability-1.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/analysis-usability-1.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['Relational design', 'Application architecture'],
+    "tech": ['YII 2' ,'mySQL'],
+    "github": [
+      {
+        "label": "Common models",
+        "URL": "https://github.com/niennte/isk2/commit/6e2ee0d3060130659068bb414838e900dd4867cb"
+      },
+      {
+        "label": "Common models with relationships and business rules",
+        "URL": "https://github.com/niennte/isk2/tree/master/common/models"
+      },
+      {
+        "label": "Entity Manager controllers",
+        "URL": "URL"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Entity manager",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com:8080"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
   {
     "id": 3,
     "title": "Relational design",
-    "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
-    "image": "/img/fullstack-entities.png",
+    "body": "Add database entities, models and relationships to provide for desired User Experience.",
+    "image": "/img/fullstack-entities-1x1000.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1x1000.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['Relational design', 'Application design', 'Application development'],
+    "tech": ['LAMP stack', 'YII 2' , 'MVC'],
+    "github": [
+      {
+        "label": "Models",
+        "URL": "https://github.com/niennte/isk2/commit/6e2ee0d3060130659068bb414838e900dd4867cb"
+      },
+      {
+        "label": "Service",
+        "URL": "https://github.com/niennte/isk2/blob/master/common/models/filter/Filters.php"
+      },
+      {
+        "label": "Entity Manager controllers",
+        "URL": "https://github.com/niennte/isk2/tree/master/backend/controllers"
+      },
+      {
+        "label": "Entity Manager views",
+        "URL": "https://github.com/niennte/isk2/tree/master/backend/views"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Entity manager \n(username 'guest', password 'password')",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com:8080"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
   {
     "id": 4,
     "title": "User experience modelling - I",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-    "image": "/img/fullstack-showcase.png",
+    "body": "World's shortest path to meaningful conversion (please click top left in the demo)",
+    "image": "/img/fullstack-showcase-3.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1x1000.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['User Experience design', 'Application development'],
+    "tech": ['LAMP Stack', 'YII 2' , 'Bootstrap 3', 'JQuery', 'AJAX'],
+    "github": [
+      {
+        "label": "Wrapper service",
+        "URL": "https://github.com/niennte/isk2/blob/master/common/models/filter/Filters.php"
+      },
+      {
+        "label": "Wrapper controller",
+        "URL": "https://github.com/niennte/isk2/tree/frontend/controllers/site"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Showcase Demo",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com"
+      },
+      {
+        "label": "Open in simulator",
+        "URL": "http://www.appsimulator.net/webapp/?frame=apple_ipad_air_h&link=http%3A%2F%2Fec2-52-87-238-76.compute-1.amazonaws.com"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
   {
     "id": 5,
     "title": "User experience modelling - II",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    "image": "/img/mobile-simulator-product.png",
+    "body": "World's shortest path to meaningful conversion (please click bottom right in the demo)",
+    "image": "/img/mobile-simulator-product-1.png",
     "imageTitle": "Screenshot of the mobile view with filter open",
-    "imageAlt": "Screenshot of the mobile view with filter open"
+    "imageAlt": "Screenshot of the mobile view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['User Experience', 'UI Design', 'Application development'],
+    "tech": ['LAMP Stack', 'YII 2' , 'JQuery Mobile', 'AJAX'],
+    "github": [
+      {
+        "label": "Wrapper service",
+        "URL": "https://github.com/niennte/isk2/blob/master/common/models/filter/Filters.php"
+      },
+      {
+        "label": "Mobile controller",
+        "URL": "https://github.com/niennte/isk2/blob/master/frontend/controllers/MobileController.php"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Mobile App Demo",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com/mobile"
+      },
+      {
+        "label": "Open in simulator",
+        "URL": "http://www.appsimulator.net/webapp/?frame=apple_iphone_6_v&link=http%3A%2F%2Fec2-52-87-238-76.compute-1.amazonaws.com%2Fmobile"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
   {
     "id": 6,
     "title": "Modular frontend design",
     "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
-    "image": "/img/frontend-presentation.png",
+    "image": "/img/frontend-presentation-1.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['Relational design', 'Application architecture'],
+    "tech": ['YII 2' ,'mySQL'],
+    "github": [
+      {
+        "label": "Common models",
+        "URL": "https://github.com/niennte/isk2/commit/6e2ee0d3060130659068bb414838e900dd4867cb"
+      },
+      {
+        "label": "Common models with relationships and business rules",
+        "URL": "https://github.com/niennte/isk2/tree/master/common/models"
+      },
+      {
+        "label": "Entity Manager controllers",
+        "URL": "URL"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Product Presentation",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com/product/iphone5s-claro"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
   {
     "id": 7,
@@ -53,7 +255,40 @@ const projectData = [
     "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque",
     "image": "/img/frontend-product-module.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['Relational design', 'Application architecture'],
+    "tech": ['YII 2' ,'mySQL'],
+    "github": [
+      {
+        "label": "Common models",
+        "URL": "https://github.com/niennte/isk2/commit/6e2ee0d3060130659068bb414838e900dd4867cb"
+      },
+      {
+        "label": "Common models with relationships and business rules",
+        "URL": "https://github.com/niennte/isk2/tree/master/common/models"
+      },
+      {
+        "label": "Entity Manager controllers",
+        "URL": "URL"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Entity manager",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com:8080"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
   {
     "id": 8,
@@ -61,7 +296,40 @@ const projectData = [
     "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae",
     "image": "/img/frontend-social-media-module.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['Relational design', 'Application architecture'],
+    "tech": ['YII 2' ,'mySQL'],
+    "github": [
+      {
+        "label": "Common models",
+        "URL": "https://github.com/niennte/isk2/commit/6e2ee0d3060130659068bb414838e900dd4867cb"
+      },
+      {
+        "label": "Common models with relationships and business rules",
+        "URL": "https://github.com/niennte/isk2/tree/master/common/models"
+      },
+      {
+        "label": "Entity Manager controllers",
+        "URL": "URL"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Entity manager",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com:8080"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
   {
     "id": 9,
@@ -69,7 +337,40 @@ const projectData = [
     "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
     "image": "/img/frontend-gallery-module.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['Relational design', 'Application architecture'],
+    "tech": ['YII 2' ,'mySQL'],
+    "github": [
+      {
+        "label": "Common models",
+        "URL": "https://github.com/niennte/isk2/commit/6e2ee0d3060130659068bb414838e900dd4867cb"
+      },
+      {
+        "label": "Common models with relationships and business rules",
+        "URL": "https://github.com/niennte/isk2/tree/master/common/models"
+      },
+      {
+        "label": "Entity Manager controllers",
+        "URL": "URL"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Entity manager",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com:8080"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
 
   {
@@ -78,7 +379,40 @@ const projectData = [
     "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
     "image": "/img/frontend-cart-module.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['Relational design', 'Application architecture'],
+    "tech": ['YII 2' ,'mySQL'],
+    "github": [
+      {
+        "label": "Common models",
+        "URL": "https://github.com/niennte/isk2/commit/6e2ee0d3060130659068bb414838e900dd4867cb"
+      },
+      {
+        "label": "Common models with relationships and business rules",
+        "URL": "https://github.com/niennte/isk2/tree/master/common/models"
+      },
+      {
+        "label": "Entity Manager controllers",
+        "URL": "URL"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Entity manager (UN 'guest', PW 'password')",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com:8080"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
   {
     "id": 11,
@@ -86,7 +420,40 @@ const projectData = [
     "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
     "image": "/img/backend-oauth2.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['Relational design', 'Application architecture'],
+    "tech": ['YII 2' ,'mySQL'],
+    "github": [
+      {
+        "label": "Common models",
+        "URL": "https://github.com/niennte/isk2/commit/6e2ee0d3060130659068bb414838e900dd4867cb"
+      },
+      {
+        "label": "Common models with relationships and business rules",
+        "URL": "https://github.com/niennte/isk2/tree/master/common/models"
+      },
+      {
+        "label": "Entity Manager controllers",
+        "URL": "URL"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Entity manager",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com:8080"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
   {
     "id": 12,
@@ -94,8 +461,49 @@ const projectData = [
     "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
     "image": "/img/backend-mcb.png",
     "imageTitle": "Screenshot of the view with filter open",
-    "imageAlt": "Screenshot of the view with filter open"
+    "imageAlt": "Screenshot of the view with filter open",
+    "posterImage": {
+      "src": "/img/fullstack-entities-1.png",
+      "title": "Screenshot of the view with filter open",
+      "alt": "Screenshot of the view with filter open"
+    },
+    "kind": ['Relational design', 'Application architecture'],
+    "tech": ['YII 2' ,'mySQL'],
+    "github": [
+      {
+        "label": "Common models",
+        "URL": "https://github.com/niennte/isk2/commit/6e2ee0d3060130659068bb414838e900dd4867cb"
+      },
+      {
+        "label": "Common models with relationships and business rules",
+        "URL": "https://github.com/niennte/isk2/tree/master/common/models"
+      },
+      {
+        "label": "Entity Manager controllers",
+        "URL": "URL"
+      }
+    ],
+    "demos": [
+      {
+        "label": "Entity manager",
+        "URL": "http://ec2-52-87-238-76.compute-1.amazonaws.com:8080"
+      }
+    ],
+    "relatedProjects": [
+      {
+        "id": 2,
+        "label": "Usability analysis"
+      }
+    ]
   },
+  {
+    "id": 15,
+    "title": "Entity - control- boundary, experimental",
+    "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
+    "image": "/img/placeholder.jpg",
+    "imageTitle": "Screenshot of the view with filter open",
+    "imageAlt": "Screenshot of the view with filter open"
+  }/*,
   {
     "id": 13,
     "title": "Entity - control- boundary, experimental",
@@ -111,7 +519,7 @@ const projectData = [
     "image": "/img/creative-animation-character.png",
     "imageTitle": "Screenshot of the view with filter open",
     "imageAlt": "Screenshot of the view with filter open"
-  }
+  }*/
 ];
 
 export default projectData;
